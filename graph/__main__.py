4a617c978ed771aeaae19ac7c8cb79d6be25b18c
Julius Pahama <pahamajulius@gmail.com>
2024-09-03 09:53:53 +0800

    revise(__main__): Revise the function that loads the css stylesheet.

--- graph/__main__.py
+++ graph/__main__.py
@@ -1,12 +1,24 @@
+import sys
+import os
 from PyQt5.QtWidgets import QApplication, QWidget 
 from graph import MainLayout
 
 # Function to load and apply the stylesheet from a file
 def load_stylesheet(file_name):
-    with open(file_name, "r") as file:
-        stylesheet = file.read()
-    return stylesheet
+     # Determine if the application is running as a script or as a bundled executable
+    if getattr(sys, 'frozen', False):  # Check if we're in a PyInstaller bundle
+        base_path = sys._MEIPASS  # Temporary folder used by PyInstaller
+    else:
+        base_path = os.path.dirname(__file__)  # Directory of the script
 
+    # Construct the full path to the stylesheet file
+    file_path = os.path.join(base_path, file_name)
+
+    # Open and read the stylesheet
+    with open(file_path, "r") as file:
+        stylesheet = file.read()
+
+    return stylesheet
 
 
 if __name__ == "__main__":
