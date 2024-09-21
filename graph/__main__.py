1644d49d51103579845581a0a87a01c776a55089
Julius Pahama <pahamajulius@gmail.com>
2024-09-21 09:58:02 +0800

    rename(cssFile)

--- graph/__main__.py
+++ graph/__main__.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     main_window.setGeometry(100, 100, 1280, 720)
     main_window.setWindowTitle("Graph Illustrator by Julius Pahama")
     main_window.setLayout(MainLayout().main_layout)
-    main_window.setStyleSheet(load_stylesheet("style/dark_mode.css"))
+    main_window.setStyleSheet(load_stylesheet("style/globals.css"))
 
     main_window.show()
 
